a123d3236fd18d4e6436724699416ce093730da0
Nurdaulet Askarmauytov <nurrdau@gmail.com>
2023-10-17 16:02:57 +0500

    Initial Commit

---
+++ Start-HW10-Askarmauytov Nurdaulet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Start-HW10-Askarmauytov Nurdaulet
+//
+//  Created by Нурдаулет on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
